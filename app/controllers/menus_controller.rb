09a5484b49352cf3db35f12b01576dc525fa4141
ethan lane <ethan.lane@outlook.co.nz>
2022-09-05 17:30:38 +0100

    menus-controller

---
+++ app/controllers/menus_controller.rb
@@ -0,0 +1,54 @@
+class MenusController < ApplicationController
+  before_action :set_restaurant, only: %i[new create edit updates]
+  before_action :set_menu, only: %i[edit update destroy]
+
+  def index
+    @menus = Menus.all
+  end
+
+  def new
+    @menu = Menu.new
+  end
+
+  def create
+    @menu = Menu.new(menu_params)
+    @menu.restaurant = @restaurant
+    # if @menu.save
+    #   redirect_to new_restaurant_menu_path(@restaurant)
+    # else
+    #   render :new, status: :unprocessable_entity
+    # end
+  end
+
+  def show
+    @menu = Menu.find(params[:id])
+  end
+
+  def edit
+    # We might also need an edit method TBD
+  end
+
+  def update
+    @menu.update(menu_params)
+    # redirect_to new_restaurant_menu_path(@restaurant)
+  end
+
+  def destroy
+    @menu.destroy
+    # redirect_to host_restaurant_path(@menu.restaurant), status: :see_other
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:name)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def set_menu
+    @menu = Menu.find(params[:id])
+  end
+end

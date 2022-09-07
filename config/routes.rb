b525d5c88ae21e98a8bd89c54d41eebc905b283e
ethan lane <ethan.lane@outlook.co.nz>
2022-09-07 13:42:29 +0100

    menu links done

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :restaurants do
-    resources :menus, only: :destroy
     resources :restaurant_servers
     resources :menu_items
-    resources :menus, except: :destroy do
+    resources :menus do
       resources :join_menus
     end
     resources :tables do

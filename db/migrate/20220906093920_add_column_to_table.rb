d754acbdf1ef9ee83142bac066ad94421fb75dc1
HaYeon Kim <kimhy8376@gmail.com>
2022-09-07 23:29:41 +0100

    organise migration files

--- db/migrate/20220906093920_add_column_to_table.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnToTable < ActiveRecord::Migration[7.0]
-  def change
-    add_column :tables, :is_active, :boolean
-  end
-end
--- db/migrate/20220906135201_remove_customer_from_table_order.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCustomerFromTableOrder < ActiveRecord::Migration[7.0]
-  def change
-    remove_reference :table_orders, :table_customer, null: false, foreign_key: true
-  end
-end

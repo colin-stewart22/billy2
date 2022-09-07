2ed0bf08f744f70c240c6db00ef9ad58912ce7f5
HaYeon Kim <kimhy8376@gmail.com>
2022-09-07 12:37:05 +0100

    Create new/create action for join-menu

--- app/controllers/join_menus_controller.rb
+++ app/controllers/join_menus_controller.rb
@@ -1,16 +1,17 @@
 class JoinMenusController < ApplicationController
-  before_action :set_menu, only: %i[new create edit update]
+  before_action :set_restaurant, only: %i[new create edit update]
+  before_action :set_menu, only: %i[show new create edit update]
   def show
-    @menu = Menu.find(params[:menu_id])
     @join_menus = JoinMenu.where(menu_id: @menu.id)
   end
 
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @join_menu = JoinMenu.new
   end
 
   def create
-    @menu_items = MenuItem.where(id: params.dig(:join_menu, :menu_item))
+    @menu_items = MenuItem.where(id: params.dig(:join_menu, :menu_item_id))
     return render_new if @menu_items.empty?
 
     ActiveRecord::Base.transaction do
@@ -18,7 +19,7 @@ class JoinMenusController < ApplicationController
         join_menu = JoinMenu.new(menu: @menu, menu_item: item)
         join_menu.save!
       end
-      redirect_to
+      redirect_to restaurant_menu_path(@restaurant, @menu)
     end
   rescue ActiveRecord::RecordInvalid
     render_new
@@ -37,6 +38,10 @@ class JoinMenusController < ApplicationController
 
   def joinmenu_params
     params.require(:join_menu).permit(menu_item_ids: [])
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
   end
 
   def set_menu

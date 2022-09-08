875c73b2964100570ed374fa5e3efabe329d07b2
HaYeon Kim <kimhy8376@gmail.com>
2022-09-08 11:50:51 +0100

    Add a route for kitchen view and add associations between models

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,14 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  before_action :set_restaurant, only: %i[show kitchen edit update destroy]
 
   def index
     @restaurants = Restaurant.all
   end
 
   def show
+  end
+
+  def kitchen
   end
 
   def new

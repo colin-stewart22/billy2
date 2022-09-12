81e20c4251b56c786ebb0236a27abca4385e5095
HaYeon Kim <kimhy8376@gmail.com>
2022-09-12 08:51:35 +0100

    add a order-items-new form

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,20 +1,43 @@
 class OrderItemsController < ApplicationController
-  before_action :set_order_item, only: %i[new create update destroy prepared! served!]
+  before_action :set_order_item, only: %i[update destroy prepared! served!]
 
   def show
   end
 
   def new
     @order_item = OrderItem.new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @table = Table.find(params[:table_id])
+    @table_order = TableOrder.find(params[:table_order_id])
+    @table_customer = TableCustomer.find(params[:table_customer_id])
   end
 
   def create
-    @order_item = OrderItem.new(order_item_params)
-    # if @order_item.save
-    #   redirect_to new_restaurant_menu_path(@restaurant)
-    # else
-    #   render :new, status: :unprocessable_entity
-    # end
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @table = Table.find(params[:table_id])
+    @table_order = TableOrder.find(params[:table_order_id])
+    @table_customer = TableCustomer.find(params[:table_customer_id])
+    @menu_items = MenuItem.where(id: params.dig(:order_item, :menu_item_id))
+    return render_new if @menu_items.empty?
+
+    ActiveRecord::Base.transaction do
+      @menu_items.each do |item|
+        @order_item = OrderItem.new(order_item_params)
+        @order_item.table_customer = @table_customer
+        @order_item.menu_item = item
+        @order_item.estimated_serving_time = item.prepare_time
+        @order_item.save!
+      end
+      redirect_to restaurant_table_table_order_table_customer_order_items_path(
+        @restaurant,
+        @table,
+        @table_order,
+        @table_customer,
+        @order_item
+      )
+    end
+  rescue ActiveRecord::RecordInvalid
+    render_new
   end
 
   def update
@@ -42,10 +65,16 @@ class OrderItemsController < ApplicationController
   private
 
   def order_item_params
-    params.require(:order_item).permit(:estimated_serving_time, :note)
+    params.require(:order_item).permit(:note, menu_item_ids: [])
   end
 
   def set_order_item
     @order_item = OrderItem.find(params[:id])
+  end
+
+  def render_new
+    @order_item = OrderItem.new
+    @order_item.errors.add(:base, "It already exists")
+    render :new, status: :unprocessable_entity
   end
 end

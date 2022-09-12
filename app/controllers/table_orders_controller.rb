d5ac0253a697ee4fd80aa88ef60962e0e2d0cf1b
ethan lane <ethan.lane@outlook.co.nz>
2022-09-12 16:17:09 +0100

    fixed redirect forms

--- app/controllers/table_orders_controller.rb
+++ app/controllers/table_orders_controller.rb
@@ -53,7 +53,7 @@ class TableOrdersController < ApplicationController
 
   def create
     @table_order = TableOrder.new(table_order_params)
-    @table_order.user = current_user
+    @table_order.user = User.where(is_owner: false).sample
     @table_order.table = @table
     if @table_order.save
       redirect_to new_restaurant_table_table_order_table_customer_path(@restaurant, @table, @table_order)

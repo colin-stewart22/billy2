b1ca747e5e616b1d245ffac77ef9ab8db7d0180d
Colin Stewart <colin.mac.stewart1@gmail.com>
2022-09-13 10:40:37 +0100

    Added checkout logic and fixed javascript

--- app/controllers/table_customers_controller.rb
+++ app/controllers/table_customers_controller.rb
@@ -69,10 +69,10 @@ class TableCustomersController < ApplicationController
           product_data: {
             name: 'T-shirt',
             description: 'Comfortable cotton t-shirt',
-            images: ['https://example.com/t-shirt.png'],
-          },
+            images: ['https://example.com/t-shirt.png']
+          }
         },
-        quantity: 1,
+        quantity: 1
       }],
       mode: 'payment',
       success_url: "http://127.0.0.1:3000/restaurants/#{@restaurant.id}/tables/#{@table.id}/table_orders/#{@table_order.id}/table_customers/#{@table_customer.id}/confirmation",
---
+++ app/javascript/controllers/order_item_modal_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="order-item-modal"
+export default class extends Controller {
+  connect() {
+    const modalSplit = document.getElementById("modal-split");
+
+    const modalPayAll = document.getElementById("modal-pay-all");
+
+    const buttonToPayAll = document.getElementById("button-to-pay-all");
+
+    const buttonToSplit = document.getElementById("button-to-split");
+
+    function handleToPayAllClick() {
+      modalSplit.classList.add("hidden")
+      modalPayAll.classList.remove("hidden")
+    }
+
+    function handleToSplitClick(){
+      modalPayAll.classList.add("hidden")
+      modalSplit.classList.remove("hidden")
+    }
+
+    buttonToPayAll.addEventListener("click", handleToPayAllClick);
+    buttonToSplit.addEventListener("click", handleToSplitClick);
+  }
+}
